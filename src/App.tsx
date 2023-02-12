79a43eec2a4a8fc91fa364ec1f57fb2d418b8809
Danish Farooq <farooq.d@northeastern.edu>
2023-02-11 22:03:42 -0500

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { ArticleApp } from "./components/ArticleApp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1 className="text-sky-400 font-josefin-sans">
-        How do you perceive the partisanship of the Supreme Court?
-      </h1>
+    <div className="bg-stone-100 h-fit-content min-h-screen min-w-full flex flex-col font-josefin-sans">
+      <header className="mx-10 align-middle justify-center basis-1/12">
+        <h1 className="text-4xl font-bold text-center mt-10 mb-5">
+          How do you perceive the partisanship of the Supreme Court?
+        </h1>
+        <hr className="bg-black h-1 mt-1 mb-3" />
+      </header>
+      <main className="grow mt-2 mx-10 my-5 basis-10/12 flex flex-col">
+        <ArticleApp />
+      </main>
+      <footer className="bg-stone-300 align-middle justify-center basis-1/12">
+        <p className="text-normal text-center my-5">
+          A project by Danish Farooq and Gabriel Cohen for JRNL 5500: Coding for
+          Digital Storytelling
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/ArticleApp.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export const ArticleApp = () => {
+  const [enteredMainArticle, setEnteredMainArticle] = useState<boolean>(false);
+
+  const handleClick = () => setEnteredMainArticle(true);
+
+  return (
+    <section className="grow text-xl text-center m-auto flex flex-col justify-center max-w-prose items-center">
+        <img src="./public/SCOTUS.png"/>
+      <p>
+        Too often, we only let the big moments define our perception of public
+        figures. For the Supreme Court Justices, that comes in the form of media
+        coverage surrounding landmark decisions and the nomination process. Many
+        will form their opinions based on talking points brought up by their
+        favorite news network, their favorite podcaster, or Twitter personality.
+        Despite the significance of these events, they do not tell the full
+        story of the voting behavior of the Justices.
+      </p>
+      <br />
+      <p>
+        So, how well do you know the partisan leanings of recent Supreme Court
+        Justices?
+      </p>
+      <br />
+      <div>
+        {/* button transitions retrieved from https://tailwind-elements.com/docs/standard/components/buttons/ */}
+        <button
+          className="border-2 border-solid bg-stone-600 p-2 text-stone-100 rounded-full border-stone-700 max-w-fit active:bg-stone-700 active:shadow-lg transition duration-150 ease-in-out "
+          onClick={handleClick}
+        >
+          {"> "} Test your perceptions now
+        </button>
+      </div>
+    </section>
+  );
+};
